df46926a9da001dd9c3455fb70eeca049fbe5769
Abdulmecid Demirhan <abdulmecid@cross4cloud.com>
2023-04-04 03:00:32 +0300

    questions became random

--- questions.js
+++ questions.js
@@ -14,63 +14,63 @@ function Question(
 
 let questions = [
   new Question(
-    1,
+    0,
     "Which of the following is a popular front-end framework for building user interfaces in JavaScript?",
     { a: "AngularJS", b: "jQuery", c: "React.js", d: "Vue.js" },
     "c",
     10
   ),
   new Question(
-    2,
+    1,
     "What is the output of the following JavaScript code?\n\nconsole.log('5' + 5 - 2);",
     { a: "52", b: "3", c: "53", d: "NaN" },
     "d",
     10
   ),
   new Question(
-    3,
+    2,
     "Which of the following is a JavaScript library commonly used for data visualization?",
     { a: "D3.js", b: "Bootstrap", c: "Ember.js", d: "React.js" },
     "a",
     10
   ),
   new Question(
-    4,
+    3,
     "Which of the following is a JavaScript runtime built on Chrome's V8 JavaScript engine?",
     { a: "Node.js", b: "Express.js", c: "Ruby on Rails", d: "Django" },
     "a",
     10
   ),
   new Question(
-    5,
+    4,
     "Which JavaScript framework is known for its two-way data binding and MVVM architecture?",
     { a: "AngularJS", b: "React.js", c: "Vue.js", d: "Backbone.js" },
     "a",
     10
   ),
   new Question(
-    6,
+    5,
     "Which of the following is a JavaScript package manager and registry?",
     { a: "npm", b: "RubyGems", c: "NuGet", d: "PyPI" },
     "a",
     10
   ),
   new Question(
-    7,
+    6,
     "Which of the following is a JavaScript testing framework?",
     { a: "Jest", b: "Jasmine", c: "Mocha", d: "Karma" },
     "c",
     10
   ),
   new Question(
-    8,
+    7,
     "Which JavaScript library is commonly used for manipulating the Document Object Model (DOM)?",
     { a: "jQuery", b: "AngularJS", c: "React.js", d: "Vue.js" },
     "a",
     10
   ),
   new Question(
-    9,
+    8,
     "Which of the following is a popular code editor for JavaScript development?",
     {
       a: "Visual Studio Code",
@@ -82,7 +82,7 @@ let questions = [
     10
   ),
   new Question(
-    10,
+    9,
     "Which JavaScript library is known for its functional programming style and immutable data structures?",
     { a: "Lodash", b: "Underscore.js", c: "Immutable.js", d: "Ramda" },
     "c",
--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const ui = new UI();
-const quiz = new Question(questions);
 
-let currentId = 0;
 let scorePoint = 0;
 let counter;
+let currentId;
+let order = 1;
 
 const saveData = () => {
   localStorage.setItem("currentId", JSON.stringify(currentId));
   localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
+};
+
+const uploadQuestions = () => {
+  let possibleQuestions = [];
+
+  for (let i = 0; i < questions.length; i++) {
+    possibleQuestions.push(questions[i].questionId);
+  }
+  localStorage.setItem("possibleQuestions", JSON.stringify(possibleQuestions));
+  for (let i; i < possibleQuestions.length; i++) {
+    order++;
+  }
 };
 
 const playAudio = (url) => {
@@ -24,6 +36,16 @@ const showCorrectAnswer = () => {
     }
   }
 };
+
+const createRandomQuestions = () => {
+  let possibleQuestions =
+    JSON.parse(localStorage.getItem("possibleQuestions")) || [];
+  const randomIdIndex = Math.floor(Math.random() * possibleQuestions.length);
+  currentId = possibleQuestions[randomIdIndex];
+  possibleQuestions.splice(randomIdIndex, 1);
+  localStorage.setItem("possibleQuestions", JSON.stringify(possibleQuestions));
+};
+
 const score = (score) => {
   let tag = `<span class="score"><i class="fas fa-star" aria-hidden="true"></i> ${score}</span>`;
   ui.scoreBoard.innerHTML = tag;
@@ -34,14 +56,12 @@ const checkAnswer = (answer) => {
 };
 
 const nextQuestion = () => {
-  if (currentId !== questions[8].questionId) {
-    currentId++;
+  if (order !== 11) {
     ui.showQuestion(currentId);
-    currentId;
     clearInterval(counter);
     localStorage.removeItem("counter");
     startTimer(questions[currentId].solveTime);
-    questionNum(questions[currentId].questionId, questions.length);
+    questionNum(order++, questions.length);
     saveData();
   } else {
     localStorage.clear();
@@ -65,20 +85,23 @@ const startTimer = (time) => {
       clearInterval(counter);
       localStorage.removeItem("counter");
       playAudio("assets/wrong.mp3");
-      localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+      createRandomQuestions();
+      localStorage.setItem("currentId", JSON.stringify(currentId));
       let answer = questions[currentId].correctAnswer;
       for (let option of ui.optionList.children) {
         if (option.querySelector("span b").textContent === answer) {
           option.classList.add("correct");
           option.insertAdjacentHTML("beforeend", ui.correctIcon);
-          localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+          localStorage.setItem("currentId", JSON.stringify(currentId));
           localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
+          localStorage.setItem("order", JSON.stringify(order));
           localStorage.removeItem("counter");
         }
         option.classList.add("disabled");
         ui.nextButton.classList.add("show");
-        localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+        localStorage.setItem("currentId", JSON.stringify(currentId));
         localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
+        localStorage.setItem("order", JSON.stringify(order));
         localStorage.removeItem("counter");
       }
     }
@@ -101,16 +124,22 @@ const optionSelected = (option) => {
     playAudio("assets/correct.mp3");
     clearInterval(counter);
     score((scorePoint += 10));
-    localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+    createRandomQuestions();
+    localStorage.setItem("currentId", JSON.stringify(currentId));
     localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
+    localStorage.setItem("order", JSON.stringify(order));
+
     localStorage.removeItem("counter");
   } else {
     option.classList.add("incorrect");
-    localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+    showCorrectAnswer();
+    createRandomQuestions();
+    localStorage.setItem("currentId", JSON.stringify(currentId));
     localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
+    localStorage.setItem("order", JSON.stringify(order));
+
     playAudio("assets/wrong.mp3");
     clearInterval(counter);
-    showCorrectAnswer();
     option.insertAdjacentHTML("beforeend", ui.incorrectIcon);
     localStorage.removeItem("counter");
   }
@@ -124,36 +153,38 @@ const optionSelected = (option) => {
 
 ui.restartButton.addEventListener("click", () => {
   localStorage.clear();
-  currentId = 0;
+  uploadQuestions();
+  createRandomQuestions();
   scorePoint = 0;
+  order = 1;
+  saveData();
   ui.resultScreen.classList.remove("active");
   ui.showQuestion(currentId);
   ui.quizBox.classList.add("active");
   startTimer(questions[currentId].solveTime);
-  questionNum(questions[currentId].questionId, questions.length);
+  questionNum(order++, questions.length);
   score(scorePoint);
 });
 
 if (
-  parseInt(localStorage.getItem("currentId")) &&
-  parseInt(localStorage.getItem("scorePoint")) !== null
+  parseInt(localStorage.getItem("currentId")) === 0 ||
+  (parseInt(localStorage.getItem("currentId")) &&
+    parseInt(localStorage.getItem("scorePoint")) !== null)
 ) {
   window.addEventListener("load", () => {
     ui.startButton.style.display = "none";
     ui.startScreen.style.display = "none";
     let id = parseInt(localStorage.getItem("currentId"));
     let lastScore = parseInt(localStorage.getItem("scorePoint"));
+    let lastOrder = parseInt(localStorage.getItem("order"));
+    possibleQuestions = parseInt(localStorage.getItem("possibleQuestions"));
     currentId = id;
     scorePoint = lastScore;
-    if (parseInt(localStorage.getItem("currentId")) === 10) {
-      ui.quizBox.classList.remove("active");
-      ui.resultScreen.classList.add("active");
-      ui.showResult(scorePoint);
-    }
+    order = lastOrder;
     ui.showQuestion(currentId);
     ui.quizBox.classList.add("active");
     startTimer(questions[currentId].solveTime);
-    questionNum(questions[currentId].questionId, questions.length);
+    questionNum(order, questions.length);
     score(scorePoint);
   });
 } else {
@@ -161,10 +192,15 @@ if (
     ui.quizBox.classList.add("active");
     ui.startButton.style.display = "none";
     ui.startScreen.style.display = "none";
-    localStorage.setItem("currentId", JSON.stringify(currentId + 1));
+    scorePoint = 0;
+    uploadQuestions();
+    createRandomQuestions();
     ui.showQuestion(currentId);
+    localStorage.setItem("currentId", JSON.stringify(currentId));
+    localStorage.setItem("order", JSON.stringify(order));
+    localStorage.setItem("scorePoint", JSON.stringify(scorePoint));
     startTimer(questions[currentId].solveTime);
-    questionNum(questions[currentId].questionId, questions.length);
+    questionNum(order++, questions.length);
     score(scorePoint);
   });
 }
